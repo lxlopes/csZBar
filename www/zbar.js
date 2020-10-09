1a1e01cf1339336ee1f5e45933cb410fae2dbb0e
lxlopes <luisxavierlopes@gmail.com>
2020-10-09 10:25:06 +0200

    Update zbar.js

--- www/zbar.js
+++ www/zbar.js
@@ -10,8 +10,7 @@ ZBar.prototype = {
         argscheck.checkArgs('*fF', 'CsZBar.scan', arguments);
 
         params = params || {};
-        //if(params.text_title === undefined) params.text_title = "Scan QR Code NOW!";
-        params.text_title = "Scan QR Code NOW!";
+        if(params.text_title === undefined) params.text_title = "Scan QR Code";
         if(params.text_instructions === undefined) params.text_instructions = "Please point your camera at the QR code.";
         if(params.camera != "front") params.camera = "back";
         if(params.flash != "on" && params.flash != "off") params.flash = "auto";
